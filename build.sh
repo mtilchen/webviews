5fab1165e3664fd51af7847e4a5ebb24b16cab97
Matthew P. Tilchen <mtilchen@gmail.com>
2010-03-05 22:03:52 -0500

    Use the correct version in the jsb2 file

--- build.sh
+++ build.sh
@@ -5,9 +5,8 @@ WV_BUILD_VERSION=`git describe --abbrev=0`
 WV_BUILD_DIR=./build/webviews-$WV_BUILD_VERSION
 
 rm -rf $WV_BUILD_DIR
-mkdir -p $WV_BUILD_DIR
 
-sed s/@VERSION@/$WV_VERSION/ < webviews.jsb2 > webviews.jsb2.tmp
+sed s/@VERSION@/$WV_BUILD_VERSION/ < webviews.jsb2 > webviews.jsb2.tmp
 java -jar ./lib/jsbuilder/JSBuilder2.jar --verbose --projectFile ./webviews.jsb2.tmp --homeDir ./build
 rm webviews.jsb2.tmp
 
