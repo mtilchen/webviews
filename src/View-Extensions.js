d6d2e02468007cc001cc63d0b30fa69e666b31ca
Matthew P. Tilchen <mtilchen@gmail.com>
2012-11-19 10:31:07 -0500

    Remove unused classes

--- src/View-Extensions.js
+++ src/View-Extensions.js
@@ -1,16 +1,4 @@
-WV.Label = WV.extend(WV.View, {
-    vtype: 'label',
-    h: 18,
-    w: 38,
-    text: '',
-    autoResizeMask: WV.RESIZE_NONE,
-    style: {
-        color: '#000',
-        fontFamily: 'Verdana',
-        fontSize: '11px',
-        fontWeight: 'normal'
-    }
-});
+
 
 WV.Link = WV.extend(WV.Label, {
     vtype: 'link',
@@ -30,74 +18,5 @@ WV.Link = WV.extend(WV.Label, {
             e.cancel();
         }
         WV.Link.superclass.click.call(this, e);
-    }
-});
-
-WV.Image = WV.extend(WV.View, {
-    vtype: 'image',
-    autoResizeMask: WV.RESIZE_NONE,
-    resizeSubviews: false,
-    preserveAspect: 'width',
-    loaded: false,
-    useNaturalSize: false,
-    naturalWidth: 0,
-    naturalHeight: 0,
-
-    setSize: function(w, h)
-    {
-        if (!this.loaded)
-        {
-            return WV.Image.superclass.setSize.call(this, w, h);
-        }
-        else if (this.preserveAspect === 'width')
-        {
-            if (typeof w === 'string')
-            {
-                w = this.convertRelative('width', w);
-            }
-            WV.Image.superclass.setSize.call(this, w, this.naturalHeight / this.naturalWidth * w);
-        }
-        else if (this.preserveAspect === 'height')
-        {
-            if (typeof h === 'string')
-            {
-                h = this.convertRelative('height', h);
-            }
-            WV.Image.superclass.setSize.call(this, this.naturalWidth / this.naturalHeight * h, h);
-        }
-        else
-        {
-            WV.Image.superclass.setSize.call(this, w, h);
-        }
-        return this;
-    },
-    setNaturalSize: function()
-    {
-        return this.setSize(this.naturalWidth, this.naturalHeight);
-    },
-    // private
-    preLoad: function()
-    {
-        // We must use the Image constructor to relaiably get the actual width/height of the image.
-        // Subsequent loads should be in the browser cache
-        var tmpImg = new Image();
-        Ext.EventManager.addListener(tmpImg, 'load', function(e, img) {
-            this.naturalHeight = tmpImg.height;
-            this.naturalWidth = tmpImg.width;
-            this.img = tmpImg;
-            tmpImg = undefined; // Don't leak
-            // TODO: Draw ourselves now?
-        }, this, { single: true });
-        tmpImg.src = this.src;
-    },
-    // private
-    load: function(src)
-    {
-        this.src = src;
-        this.img = undefined;
-        this.loaded = false;
-        this.preLoad();
-
-        return this;
     }
 });
